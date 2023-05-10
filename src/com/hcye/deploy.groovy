88fa76329bc1956a78a157aa52f57f29d4cfcb1d
Administrator <413351397@qq.com>
2023-05-10 17:22:55 +0800

    asdasd

--- src/com/hcye/deploy.groovy
+++ src/com/hcye/deploy.groovy
@@ -11,6 +11,7 @@ def start(){
     try{
         //env.CURRENT_IMAGE用来存储当前构建的镜像地址，需要在Docker.groovy中设置值
         sh "sed -i 's#{{IMAGE_URL}}#${env.CURRENT_IMAGE}#g' ${this.resourcePath}/*"     //  CURRENT_IMAGE
+        echo ${env.CURRENT_IMAGE}
         sh "kubectl apply -f ${this.resourcePath}"
         updateGitlabCommitStatus(name: env.STAGE_NAME, state: 'success')
 //        this.msg.updateBuildMessage(env.BUILD_TASKS, "${env.stage_name} OK...  √")
