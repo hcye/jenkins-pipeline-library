ed36e5e522749ca1301ab5dcb718620e9375fd4c
Administrator <413351397@qq.com>
2023-05-10 18:06:37 +0800

    adsad

--- src/com/hcye/deploy.groovy
+++ src/com/hcye/deploy.groovy
@@ -12,9 +12,13 @@ def start(){
         //env.CURRENT_IMAGE用来存储当前构建的镜像地址，需要在Docker.groovy中设置值
         sh "sed -i 's#{{ELADMIN-IMAGE}}#${env.CURRENT_IMAGE}#g' ${this.resourcePath}/*"
         sh "cat ${this.resourcePath}/eladmin-api.yaml"
-        print('asdasdasdasd-----')
         //  CURRENT_IMAGE
-        sh "kubectl apply -f ${this.resourcePath}/*"
+        sh
+        def proc = "kubectl apply -f ${this.resourcePath}/".execute()
+        def b = new StringBuffer()
+        proc.consumeProcessErrorStream(b)
+        println proc.text
+        println b.toString()
         updateGitlabCommitStatus(name: env.STAGE_NAME, state: 'success')
 //        this.msg.updateBuildMessage(env.BUILD_TASKS, "${env.stage_name} OK...  √")
     } catch (Exception exc){
